28a440157c676e1aef7a0d065bd77e3bc449950a
574411705 <574411705@qq.com>
2020-10-07 20:31:12 +0200

    add the PHY-change function by pulling button

--- ble_app_att_mtu_throughput/pca10056/s140/ses/Output/ble_app_att_mtu_throughput_pca10056_s140 Release/Obj/fnmatch.d
+++
@@ -1,5 +0,0 @@
-Output/ble_app_att_mtu_throughput_pca10056_s140\ Release/Obj/fnmatch.o: \
- E:\nRF5_SDK_17.0.0_9d13099\external\fnmatch\fnmatch.c \
- E:/SEGGER/include/ctype.h E:/SEGGER/include/__crossworks.h \
- E:\nRF5_SDK_17.0.0_9d13099\external\fnmatch\fnmatch.h \
- E:/SEGGER/include/string.h E:/SEGGER/include/__crossworks.h
--- ble_app_att_mtu_throughput/pca10056/s140/ses/Output/ble_app_att_mtu_throughput_pca10056_s140 Release/Obj/utf.d
+++
@@ -1,5 +0,0 @@
-Output/ble_app_att_mtu_throughput_pca10056_s140\ Release/Obj/utf.o: \
- E:\nRF5_SDK_17.0.0_9d13099\external\utf_converter\utf.c \
- E:\nRF5_SDK_17.0.0_9d13099\external\utf_converter\utf.h \
- E:/SEGGER/include/stddef.h E:/SEGGER/include/__crossworks.h \
- E:/SEGGER/include/stdint.h
